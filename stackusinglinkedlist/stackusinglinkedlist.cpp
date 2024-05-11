bb3207a47c7d4ed212b799cef9edbfe36ef36810
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-05-11 10:17:16 +0700

    menambahkan kondisi if

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -34,7 +34,13 @@ public:
 
 	void pop()
 	{
-		
+		if (isEmpty())
+		{
+			cout << "Stack is empty." << endl;
+			return;
+		}
+		cout << "Popped value: " << top->data << endl;
+		top = top->next;
 	}
 };
 
