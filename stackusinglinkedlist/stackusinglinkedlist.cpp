9e9ecf5fabb06b567428647d3c1928e7313c5e56
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-05-11 10:24:23 +0700

    selesaiiiiiiiiiiiiiiiiiiiii

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -107,5 +107,11 @@ int main() {
 		case 4:
 			cout << "Exiting program." << endl;
 			break;
+		default:
+			cout << "Invalid choice. Try again." << endl;
+			break;
+		}
+		cout << endl;
 	}
+	return 0;
 }
